7331912621bf1e9d9e802bc055edad82b2d6bb79
Abdiyu <abdiyu16668@gmail.com>
2023-06-07 15:17:19 +0300

    reversed

--- apiproject/settings.py
+++ apiproject/settings.py
@@ -85,7 +85,6 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -172,7 +171,6 @@ MEDIA_URL = 'media/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
