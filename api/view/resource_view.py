11605f51e09981b909b139ab169d411b88b6aae3
Abdiyu <abdiyu16668@gmail.com>
2023-06-24 03:10:03 +0300

    enyewesti

--- api/view/resource_view.py
+++ api/view/resource_view.py
@@ -225,3 +225,91 @@ def declineResource(request):
     except Exception as e:
         print (e)
         return Response(e)
+    
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+@allowed_users(allowed_rolls=['federal','region','zone','woreda','kebele_admin'])
+def acceptResource(request):
+    try:
+        data = request.data
+        print(data)
+
+        response = JWT_authenticator.authenticate(request)
+        request_user , token = response
+        user=User.objects.get(id=request_user.id)
+        print(data)
+        sentresource = SentResource.objects.filter(id=data['resource_id']).first()
+        resource_old=Resource.objects.filter(user=user , name=sentresource.name).first()
+
+        if resource_old:
+            print("user have it already have this resource so only update the amount")
+            resource_old.amount=int(resource_old.amount)+int(sentresource.amount)
+            resource_old.save()
+            print("uih delete from sent recource")
+            sentresource.delete()
+            rec=SentResource.objects.filter(reciever=user.id)
+            serializer = SentResourceSerializer(rec, many=True)
+            return Response(serializer.data)
+        else:
+            print("user dont have this resource tpe so create new field for him")
+            print(sentresource.name,sentresource.type,sentresource.price_perKilo,sentresource.amount,)
+            newresource = Resource.objects.create(
+            name=sentresource.name,
+            type=sentresource.type,
+            amount=sentresource.amount,
+            price_perKilo=sentresource.price_perKilo,
+            user=user,
+        )    
+            print(newresource)
+            print("uih delete from sent recource")
+            sentresource.delete()
+            rec=SentResource.objects.filter(reciever=user.id)
+            serializer = SentResourceSerializer(rec, many=True)
+            return Response(serializer.data)
+    except Exception:
+        return Response(Exception)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+@allowed_users(allowed_rolls=['federal','region','zone','woreda','kebele_admin'])
+def cancelResource(request):
+    try:
+        data = request.data
+        response = JWT_authenticator.authenticate(request)
+        request_user , token = response
+        user=User.objects.get(id=request_user.id)
+        sentresource = SentResource.objects.filter(id=data['resource_id']).first()
+        print("rreached here",sentresource.sender,"\n\n")
+
+        sender=User.objects.get(username=sentresource.sender)
+        resource_old=Resource.objects.filter(user=sender , name=sentresource.name).first()
+        print("reached here")
+        if resource_old:
+            print("user have it already have this resource so only update the amount")
+            resource_old.amount=int(resource_old.amount)+int(sentresource.amount)
+            resource_old.save()
+            print("uih delete from sent recource")
+            sentresource.delete()
+            rec=SentResource.objects.filter(sender=user.id)
+            serializer = SentResourceSerializer(rec, many=True)
+            return Response(serializer.data)
+        else:
+            print("user dont have this resource tpe so create new field for him")
+            newresource = Resource.objects.create(
+            name=sentresource.name,
+            type=sentresource.type,
+            amount=sentresource.amount,
+            price_perKilo=sentresource.price_perKilo,
+            user=sender,
+        )    
+            print(newresource)
+            print("uih delete from sent recource")
+            sentresource.delete()
+            rec=SentResource.objects.filter(sender=user.id)
+            serializer = SentResourceSerializer(rec, many=True)
+            return Response(serializer.data)
+    except Exception as e:
+        print (e)
+        return Response(e)
